c7e8c5148d09624cd72b480da59f85fc5815a12d
joeycurran <47217415+joeycurran@users.noreply.github.com>
2019-10-06 18:57:34 +0100

    Added tests for getNode and putNode

--- SWENG_A1/lowestCommonAncestorTest.java
+++ SWENG_A1/lowestCommonAncestorTest.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 
-
+@RunWith(JUnit4.class)
 
 public class lowestCommonAncestorTest {
 	
@@ -20,10 +20,55 @@ public class lowestCommonAncestorTest {
 		lowestCommonAncestor<Integer, Integer> lca = new lowestCommonAncestor<Integer, Integer>();
 		assertFalse("Checking if empty tree contains key ", lca.existsCheck(3));
 		assertNull("Checking if we can get value from empty tree ", lca.getNode(0));
+		assertEquals("Checking if we can find lca for empty tree ",null, lca.getLowestCommonAncestor(0,0));
+		assertEquals("Checking if we can get node for empty tree", null, lca.getNode(3));
+
+
+		
+	}
+	
+	@ Test
+	
+	public void putNodeTest() {
+		
+		
+		lowestCommonAncestor<Integer, Integer> lca = new lowestCommonAncestor<Integer, Integer>();
+		
+		lca.putNode(1, 1);
+		assertTrue("Checking if tree contains key ", lca.existsCheck(1));
+		lca.putNode(2,  2);
+		lca.putNode(100, 345555);
+		assertTrue("Checking if tree contains key ", lca.existsCheck(100));
+		assertFalse("Checking if tree contains key ", lca.existsCheck(0));
+		lca.putNode(null,  null);
+		assertFalse("Checking if tree contains a null key ", lca.existsCheck(null));
+		
 		
 		
 		
 	}
+	
+	@ Test
+	
+	public void getNodeTest() {
+		lowestCommonAncestor<Integer, Integer> lca = new lowestCommonAncestor<Integer, Integer>();
+		
+		lca.putNode(1, 1);
+		lca.putNode(2, 2);
+		lca.putNode(3, 5);
+		lca.putNode(4, 4);
+		lca.putNode(5, 5);
+		lca.putNode(6, 6);
+		lca.putNode(7, 7);
+		
+		assertEquals("Checking if we can find lca for this tree", (Integer)5, lca.getNode(3));
+		assertEquals("Checking if we can find lca for this tree", null, lca.getNode(8));
+
+		
+		
+		
+	}
+
 
 	
 
